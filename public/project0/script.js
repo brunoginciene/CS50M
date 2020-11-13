689db2342e29ee848273f7186ce57e6f07eb4cc3
Bruno Rodrigues Ginciene <brunoginciene@hotmail.com>
2020-11-13 15:07:30 +0000

    project0

--- public/project0/script.js
+++ public/project0/script.js
@@ -10,5 +10,29 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+    let enteredText = document.getElementById("textEntry")
+
+    let item = document.createElement("li")
+    let container = document.createElement("div")
+    let checkbox = document.createElement("input")
+    let todoText = document.createElement("p")
+    let btnDelete = document.createElement("button")
+
+    container.setAttribute("class", classNames.TODO_ITEM)
+    checkbox.setAttribute("class", classNames.TODO_CHECKBOX)
+    checkbox.setAttribute("type", "checkbox")
+    todoText.setAttribute("class", classNames.TODO_TEXT)
+    btnDelete.setAttribute("class", classNames.TODO_DELETE)
+
+    todoText.innerHTML = enteredText.value
+    btnDelete.innerHTML = "Delete"
+
+    container.appendChild(checkbox)
+    container.appendChild(todoText)
+    container.appendChild(btnDelete)
+    item.appendChild(container)
+    list.appendChild(item)
+
+    enteredText.value = ""
+    
 }
